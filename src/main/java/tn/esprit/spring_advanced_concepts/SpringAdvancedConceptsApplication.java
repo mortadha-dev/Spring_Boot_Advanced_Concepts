7fd8d40f697be2af16a35b6e984ffcf850911fc7
mortadha <mortadha.bouallagui1@esprit.tn>
2021-09-25 17:00:41 +0200

    initialise the project

---
+++ src/main/java/tn/esprit/spring_advanced_concepts/SpringAdvancedConceptsApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring_advanced_concepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdvancedConceptsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdvancedConceptsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/spring_advanced_concepts/SpringAdvancedConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.spring_advanced_concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvancedConceptsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
